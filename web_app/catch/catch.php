4fac68fb79024addd0cc14d8f287b51c7d615af2
maxtaoge <2363359455@qq.com>
2016-06-09 14:13:31 +0800

    simple_html

--- web_app/catch/catch.php
+++ web_app/catch/catch.php
@@ -1,14 +1,17 @@
 <?php
-<?php
-取得指定位址的內容，並储存至text
-$text=file_get_contents('http://yourweb/');
 
-去除換行及空白字元（序列化內容才需使用）
+//取得指定位址的內容，並储存至text
+$text=file_get_contents('http://blog.sina.com.cn/s/blog_6ad6243801016dmi.html');
+
+//去除換行及空白字元（序列化內容才需使用）
 $text=str_replace(array("\r","\n","\t","\s"), '', $text); 
+$pattern='#<div[^<>]*>(([^<>]*|(?R))*)</[^<>]*>#';
+preg_match_all($pattern,$text,$match);
+//取出div标签且id為PostContent的內容，並储存至阵列match
+//preg_match('/<div[^>]*id="sinablogHead"[^>]*>(.*?) <\/div>/',$text,$match);
 
-取出div标签且id為PostContent的內容，並储存至阵列match
-preg_match('/<div[^>]*id="PostContent"[^>]*>(.*?) <\/div>/si',$text,$match);
-
-印出match[0]
+//印出match[0]
 print($match[0]);
+//preg_match('/<div[^>]*id="headflash"[^>]*><\/div>/',$match[0],$match);//(.*?) 
+//print($match[0]);
 ?>
